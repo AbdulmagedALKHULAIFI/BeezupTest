a250283467776c9f7800f89fc65436a17f1128bc
AbdulmagedALKHULAIFI <43535031+AbdulmagedALKHULAIFI@users.noreply.github.com>
2022-07-28 15:41:47 +0200

    Add project files.

---
+++ BeezupTest/Controllers/FilesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeezupTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilesController : ControllerBase
+    {
+        private readonly ILogger<FilesController> _logger;
+
+        public FilesController(ILogger<FilesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet,Route("Files")]
+        public string GetFile()
+        {
+            return "returned value";
+        }
+    }
+}
